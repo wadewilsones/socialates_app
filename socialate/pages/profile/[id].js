1e42c7f1daa60af57fea08ec2efabc6022291400
Ulada <uladaharanina@gmail.com>
2022-10-29 12:14:33 -0400

    Photos, post layout is done

--- socialate/pages/profile/[id].js
+++ socialate/pages/profile/[id].js
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router'; // tp get access to parameter in url
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import styles from "../../styles/Profile.module.css";
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import SchoolIcon from '@mui/icons-material/School';
+import InfoIcon from '@mui/icons-material/Info';
 
 const Profile = () => {
     const router = useRouter();
@@ -19,7 +22,38 @@ const Profile = () => {
                 <h3>Christine Warner</h3>
                 <h6>Some meaningful status</h6>
             </section>
-        <Footer></Footer>
+
+            <section className={styles.ContactInfo}>
+            <h3>Contact Info</h3>
+                <ul>
+                    <li><ApartmentIcon className = {styles.contactIcons}></ApartmentIcon><span>City: Greensboro</span></li>
+                    <li><SchoolIcon  className = {styles.contactIcons}></SchoolIcon><span>Education: High School of Greensboro</span></li>
+                </ul>
+                
+                <a><InfoIcon id = {styles.contactIconsInfo}></InfoIcon><span>Show more information</span></a>
+            </section>
+
+            <section  className={styles.Photos}>
+                <h3>Photos <span>120</span></h3>
+                <div>
+                    <img src='https://cdn.pixabay.com/photo/2022/10/01/22/38/sugarloaf-rock-7492389_960_720.jpg'></img>
+                    <img src='https://cdn.pixabay.com/photo/2022/10/09/04/28/deer-7508187_960_720.jpg'></img>
+                    <img src='https://cdn.pixabay.com/photo/2022/09/15/09/59/water-7456116_960_720.jpg'></img>
+                </div>
+            </section>
+
+            <section className={styles.CreatePost}>
+            <h3>Create a Post</h3>
+                <div>
+                    <input type="text" placeholder="Got something to say?"></input>
+                    <label for ="uploadImage">
+                        <input type="file" id='uploadImage' hidden/>
+                        <img src='/images/uploadPic.svg' />
+                    </label>
+                </div>
+
+            </section>
+   
     </div>;
 }
 
